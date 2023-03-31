b7a902c2ae456824435cc275ef98eb3342405038
clean17 <piw940317@gmail.com>
2023-03-31 21:40:06 +0900

    채팅창 완성

--- lib/components/chat_icon_button.dart
+++ lib/components/chat_icon_button.dart
@@ -1,2 +1,17 @@
 import 'package:flutter/material.dart';
 
+class ChatIconButton extends StatelessWidget {
+  const ChatIconButton({Key? key, required this.icon}) : super(key: key);
+
+  final icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: () {},
+      icon: icon,
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      iconSize: 25,
+    );
+  }
+}
--- lib/screens/chat_room_screen.dart
+++ lib/screens/chat_room_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_kakao/components/chat_icon_button.dart';
 import 'package:flutter_kakao/components/other_chat.dart';
 import 'package:flutter_kakao/components/time_line.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:intl/intl.dart';
 
 import '../components/my_chat.dart';
 import '../models/chat.dart';
@@ -52,10 +54,48 @@ class _ChatRoomScreenState extends State<ChatRoomScreen> {
                       ...List.generate(chats.length, (index) => chats[index]),
                     ],
                   )),
+            ),  // 익스펜디드가 존재하므로 나머지 영역을 벽에 붙어서 만듦
+            Container(
+              height: 60,
+              color: Colors.white,
+              child: Row(
+                children: [
+                  ChatIconButton(icon: Icon(FontAwesomeIcons.plusSquare)),
+                  Expanded(
+                    child: Container(
+                      child: TextField( // 이 속성은 ? 영역을 필요로 하는가 ?
+                        controller: _textEditingController, // 알아봐
+                        maxLines: 1, // 옆으로 길어질듯 ?
+                        style: TextStyle(fontSize: 20),
+                        decoration: InputDecoration( // 이것도 알아보구
+                          focusedBorder: InputBorder.none,
+                          enabledBorder: InputBorder.none,
+                        ),
+                        onSubmitted: _handleSubmitted,   // 이 옵션 공부좀
+                    )
+                    ),
+                  ),
+                  ChatIconButton(icon: Icon(FontAwesomeIcons.smile)), // 위에 익스펜디드가 영역 다 먹음 
+                  ChatIconButton(icon: Icon(FontAwesomeIcons.cog)),
+                ]
+              ),
             )
           ],
         ),
       ),
     );
+  }
+
+  void _handleSubmitted(text) {
+    _textEditingController.clear(); // 전송하면 비워줌
+
+    setState(() {
+      chats.add(  // 생성자로 만들고 추가 - 다시그림 )
+        MyChat(text: text, time: DateFormat("a k:m").format(new DateTime.now()) // "a k:m" 은 뭐임 ? - int 패키지를 이용한 날짜 포맷 ?
+                                      .replaceAll("AM", "오전")
+                                      .replaceAll("PM", "오후"),
+        ),
+      );
+    });
   }
 }
